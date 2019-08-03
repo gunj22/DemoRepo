9a646d4fbf458789f2853c8a9212a67fe565cd74
gunj22 <sam_bhasin4@yahoo.com>
2019-08-03 22:23:54 +0530

    Second modifiaction by sumit

--- src/main/java/com/qa/pages/TestGIt.java
+++ src/main/java/com/qa/pages/TestGIt.java
@@ -1,5 +1,10 @@
 package com.qa.pages;
 
+import org.omg.Messaging.SyncScopeHelper;
+
 public class TestGIt {
+	public static void main(String[] args) {
+		System.out.println("For Testing Purpose");
+	}
 
 }
